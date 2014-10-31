61d4580b5a0a7a4fd2fc0296e30825b4bd7cf892
amassiro <massironi.andrea@gmail.com>
2014-10-31 17:18:03 +0100

    removal of unused program

--- test/H4data2text.cpp
+++ test/H4data2text.cpp
@@ -10,10 +10,10 @@
 #include "include/TBEvent.h"
 
 //---- boost
-#include "boost/program_options.hpp"
-#include "boost/program_options/options_description.hpp"
+// #include "boost/program_options.hpp"
+// #include "boost/program_options/options_description.hpp"
 
-namespace po = boost::program_options;
+// namespace po = boost::program_options;
 
 
 int main(int argc, char**argv){
@@ -23,40 +23,71 @@ int main(int argc, char**argv){
 
  //---- configuration
  
- // Declare the supported options.
- po::options_description desc("Allowed options");
- desc.add_options()
-    ("help", "produce help message")
-    ("input_file",  po::value<std::string>(), "input file")
-    ("output_file", po::value<std::string>(), "output file")
- ;
+ //---- configuration
  
- po::variables_map vm;
- po::store(po::parse_command_line(argc, argv, desc), vm);
- po::notify(vm);
- 
- if (vm.count("help")) {
-  std::cout << desc << "\n";
-  return 1;
- }
- 
- if (vm.count("input_file")) {
-  input_file = vm["input_file"].as<std::string>();
- } else {
-  std::cout << "defult?.\n";
- }
- 
- if (vm.count("output_file")) {
-  output_file = vm["output_file"].as<std::string>();
- } else {
-  std::cout << "defult?.\n";
- }
+ int c;
+ while ((c = getopt (argc, argv, "i:o:m:")) != -1)
+  switch (c)
+  {
+   case 'i':
+    input_file = string(optarg);
+    break;
+   case 'o':
+    output_file =  string(optarg);
+    break;
+    
+   case '?':
+    if (optopt == 'i'|| optopt == 'o')
+     fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+    else if (isprint (optopt))
+     fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+    else
+     fprintf (stderr,
+              "Unknown option character `\\x%x'.\n",
+              optopt);
+     return 1;
+   default:
+    exit (-1);
+  }
+  
+  
+  
+  
+//  // Declare the supported options.
+//  po::options_description desc("Allowed options");
+//  desc.add_options()
+//     ("help", "produce help message")
+//     ("input_file",  po::value<std::string>(), "input file")
+//     ("output_file", po::value<std::string>(), "output file")
+//  ;
+//  
+//  po::variables_map vm;
+//  po::store(po::parse_command_line(argc, argv, desc), vm);
+//  po::notify(vm);
+//  
+//  if (vm.count("help")) {
+//   std::cout << desc << "\n";
+//   return 1;
+//  }
+//  
+//  if (vm.count("input_file")) {
+//   input_file = vm["input_file"].as<std::string>();
+//  } else {
+//   std::cout << "defult?.\n";
+//  }
+//  
+//  if (vm.count("output_file")) {
+//   output_file = vm["output_file"].as<std::string>();
+//  } else {
+//   std::cout << "defult?.\n";
+//  }
  
  
  //---- configuration (end)
  
- TBSpill tbspill;
- TBEvent tbevent;
+ //--- it works, but if I activate I need to link to libTB library, and to keep it simple I don't
+//  TBSpill tbspill;
+//  TBEvent tbevent;
  
  
  //---- read file
@@ -91,11 +122,11 @@ int main(int argc, char**argv){
  
  TFile* output_file_root = new TFile ("out.root","RECREATE");
  TTree* outtree = new TTree("t1041","t1041");
- outtree->Branch("tbspill", "TBSpill", &tbspill, 64000, 0);
- outtree->Branch("tbevent", "TBEvent", &tbevent, 64000, 0);
+//  outtree->Branch("tbspill", "TBSpill", &tbspill, 64000, 0);
+//  outtree->Branch("tbevent", "TBEvent", &tbevent, 64000, 0);
  
  for (int i=0; i<nEntries; i++) {
-  tbspill.SetSpillData(v_spillNumber[i], 0, 0, 0);
+//   tbspill.SetSpillData(v_spillNumber[i], 0, 0, 0);
   outtree->Fill();
  }
  outtree->Write();
