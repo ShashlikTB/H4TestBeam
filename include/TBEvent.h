6f6a3248c00aa23a49e5adeea4083a27dcda4745
Bob Hirosky <bob.hirosky@gmail.com>
2014-11-01 14:09:42 -0400

    new channel mapping

--- include/TBEvent.h
+++ include/TBEvent.h
@@ -138,7 +138,8 @@ class TBEvent : public TObject {
   static const ULong64_t START_H4TB=635479530091849620L;  // before installation
   static const ULong64_t H4TB_MAP1=635502232652093170L;   // first data configuration 29-Oct-2014
   static const ULong64_t H4TB_MAP2=635502992419392830L;   // 2nd data configuration 30-Oct-2014
-  static const ULong64_t H4TB_MAP3=635503713451583370L;   // 3rd data configuration 3-Oct-2014
+  static const ULong64_t H4TB_MAP3=635503713451583370L;   // 3rd data configuration 31-Oct-2014
+  static const ULong64_t H4TB_MAP4=635504599999999999L;   // 4th data configuration 01-Nov-2014
 
 
   /// Times when change in pulse shapes occur
